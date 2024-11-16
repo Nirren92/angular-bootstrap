9291d34e70dd6360f9cbe920058e5864cbab0beb
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-11-16 22:17:53 +0100

    testar att tillåta 10 warningar

--- src/app/mainmenu/mainmenu.component.ts
+++ src/app/mainmenu/mainmenu.component.ts
@@ -2,14 +2,14 @@ import {Component} from '@angular/core';
 
 import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 @Component({
   selector: 'app-mainmenu',
   standalone: true,
   imports: [CommonModule,RouterLink, RouterLinkActive],
   templateUrl: './mainmenu.component.html',
-  styleUrl: './mainmenu.component.scss'
+  styleUrls: ['./mainmenu.component.scss']
 })
 export class MainmenuComponent {
 
