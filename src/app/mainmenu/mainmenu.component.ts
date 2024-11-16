e65eff97341863870106b3c41a3889ba5b73040b
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-11-16 21:29:31 +0100

    test igen

--- src/app/mainmenu/mainmenu.component.ts
+++ src/app/mainmenu/mainmenu.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 @Component({
   selector: 'app-mainmenu',
