a5844357a92c6bc1255dbcef754bb9ff00c6447b
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-11-18 21:51:59 +0100

    klart :)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { StartComponent } from './start/start.component';
 import { Ramverk1Component } from './ramverk1/ramverk1.component';
 import { Ramverk2Component } from './ramverk2/ramverk2.component';
+import { AzureComponent } from './azure/azure.component';
 
 export const routes: Routes = [
    { path:'start', component:StartComponent},
    { path:'ramverk1', component:Ramverk1Component},
    { path:'ramverk2', component:Ramverk2Component},
+   { path:'azure', component:AzureComponent},
    {path:'', redirectTo:'/start', pathMatch:'full'}
 ];
